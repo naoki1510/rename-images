b24efc7a6da6d5afbb0af95df28eabc00301e658
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2024-11-27 00:45:32 +0900

    時間を含められるように

--- src/utils/getTargetDir.ts
+++ src/utils/getTargetDir.ts
@@ -16,10 +16,16 @@ export const getTargetDir = (info: {
   const monthStr = date ? String(month).padStart(2, '0') : '';
   const day = date.getDate();
   const dayStr = date ? String(day).padStart(2, '0') : '';
+  const hour = date.getHours();
+  const hourStr = date ? String(hour).padStart(2, '0') : '';
+  const minute = date.getMinutes();
+  const minuteStr = date ? String(minute).padStart(2, '0') : '';
 
   return TARGET_DIR + TARGET_FILENAME.replaceAll(/%year%/gi, yearStr)
     .replaceAll(/%month%/gi, monthStr)
     .replaceAll(/%day%/gi, dayStr)
+    .replaceAll(/%hour%/gi, hourStr)
+    .replaceAll(/%minute%/gi, minuteStr)
     .replaceAll(/%model%/gi, model)
     .replaceAll(/%lens%/gi, lens);
 };
